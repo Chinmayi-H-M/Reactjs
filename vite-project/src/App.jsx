81acf5d3bd62768f3eff7902a988dd90edd48c1d
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-10 10:28:23 +0530

    Heelo from jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,18 @@
-function App(){
-  return 'hello'
-  return <h1>This is from app.jsx</h1>
+// function App(){
+//   return 'hello'
+//   return <h1>This is from app.jsx</h1>
+// }
+// export default App
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <>
+      //empty tags are used to reurn multiple divs
+      </>
+    </div>
+  )
 }
+
 export default App
