a28c338e0fef77a45b1a15d3f9924c4cf3ced957
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-11 17:13:03 +0530

    Components usage then call the one function in another

--- vite-project-2/src/App.jsx
+++ vite-project-2/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Card from './card'
+import Card from './components/card'
+import Navbar from './components/navbar'
 
 const App = () => {
   const user = "Chinmayi"
   const age= 20
   return (
     <div>
-      
+      <Navbar/>
       <Card />
     </div>
   )
--- vite-project-2/src/components/card.jsx
+++ vite-project-2/src/components/card.jsx
@@ -1,8 +1,15 @@
-function Card () {
-  return <div classname='card'>
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div classname='card'>
     <h1>Chinmayi H M</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe iure sapiente labore minima neque illo vero dolor sint laborum eum?</p>
   </div>
+    </div>
+  )
 }
 
-export default Card;
+export default card
+
