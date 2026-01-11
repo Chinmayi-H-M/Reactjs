db7323633fae6f7e71270a773b200ee4d26d0510
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-11 16:54:01 +0530

    Variable access inside the jsx file

--- vite-project-2/src/App.jsx
+++ vite-project-2/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const App = () => {
+  const user = "Chinmayi"
+  const age= 20
   return (
     <div>
-      
+      <h1>Hello, guys i am {user}</h1>
+      <p>I am {age} year old.</p>
     </div>
   )
 }
