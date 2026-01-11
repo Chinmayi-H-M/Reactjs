1cccb9f81b2360cce000859ecd24affd044b4cb0
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-11 18:20:32 +0530

    Profile visiting cards

--- vite-project-03/src/App.jsx
+++ vite-project-03/src/App.jsx
@@ -4,7 +4,9 @@ import Profile from './components/profile'
 const App = () => {
   return (
     <div className = "parent">
-      <Profile user="Chinmayi" age={18}/>// numbers has to be passed using curly braces only
+      <Profile user="Chinmayi" age={18} img='https://images.unsplash.com/photo-1767041573027-f77c33df6b7c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      {/* numbers has to be passed using curly braces only */}
+      <Profile user="Harshitha" age={18} img='https://plus.unsplash.com/premium_photo-1753982281905-1b21bca9682e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     </div>
   )
 }
--- vite-project-03/src/components/profile.jsx
+++ vite-project-03/src/components/profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const profile = () => {
+const profile = (props) => {
   return (
       <div className="card">
-        <img src="https://images.unsplash.com/photo-1767041573027-f77c33df6b7c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Chinmayi H M</h1>
+        <img src={props.img} alt="" />
+        <h1>{props.user}</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus adipisci harum quis, hic accusamus non nulla cumque impedit vel sint?</p>
         <button>View Profile</button>
       </div>
