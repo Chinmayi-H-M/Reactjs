85ae1d34c1a88eef448c79df49f6436d4abae580
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-11 19:04:27 +0530

    Card ui

---
+++ vite-project-04/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img src="" alt="" />
+          <button></button>
+        </div>
+        <div className="center">
+          <h2>amazon</h2>
+        </div>
+        <div className="bottom">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
