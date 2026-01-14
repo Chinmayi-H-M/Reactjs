e2f657ce2f7ba53c441f5a375e7869fcc60025d8
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-14 16:24:42 +0530

    Created files to undertsand module stricture

---
+++ vite-project-04/src/components/button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from'./button.module.css'
+
+const button = () => {
+  return (
+    <div className='{styles.btn}'>
+        This is a button
+      
+    </div>
+  )
+}
+
+export default button
