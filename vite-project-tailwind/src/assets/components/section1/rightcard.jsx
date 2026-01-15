6ddba00aff7105e9b1f096c51d302dd2451134b8
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-15 15:19:48 +0530

    Desiging pages

---
+++ vite-project-tailwind/src/assets/components/section1/rightcard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const rightcard = () => {
+  return (
+    <div className='h-full w-80 rounded-xl relative overflow-hidden'>
+      <img className='h-full w-full object-cover' src="" alt="" />
+    </div>
+  )
+}
+
+export default rightcard
