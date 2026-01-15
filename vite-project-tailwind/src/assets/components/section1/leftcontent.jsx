48988f78b9e5eabbb624d4d297b9aa8aead10c42
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-15 10:09:19 +0530

    Desiging pages

--- vite-project-tailwind/src/assets/components/section1/leftcontent.jsx
+++ vite-project-tailwind/src/assets/components/section1/leftcontent.jsx
@@ -4,9 +4,10 @@ import { MoveUpRight } from "lucide-react";
 
 const leftcontent = () => {
   return (
-    <div className='h-full w-1/3 bg-blue-600'>
+    <div className='h-full w-1/3 bg-blue-600 '>
       <div>
-
+         <h3 className='text-6xl font-bold'>Prospective <br/><span>Customer</span><br />Segametation<br /> </h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aspernatur ullam consectetur nostrum modi molestias mollitia cupiditate aliquid impedit deserunt.</p>
       </div>
       <div>
         <MoveUpRight  size={64}/>
