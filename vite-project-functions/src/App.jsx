4171f8982b2816f7f51cf83be8c4125cb4578edf
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-16 15:12:08 +0530

    Functions in react

---
+++ vite-project-functions/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+
+  function btnClicked(){
+    console.log("Button is clicked");
+  }
+  return (
+    <div>
+      <h1>Heelo. Chinmayi</h1>
+      {/* mouse entered  */}
+      <button onclick={btnClicked}>Change user</button>
+      <button onclick={function(){
+         console.log("button is clicked");
+
+      }}>Click here</button>
+      <button onClick={()=>{
+        console.log("Clicked from arrow function");
+      }}>Click here arrow function</button>
+    </div>
+  )
+}
+
+export default App
