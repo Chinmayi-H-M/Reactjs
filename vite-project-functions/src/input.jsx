51b9f9333d6e6df4d39043883926653fa3c0a791
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-16 15:21:42 +0530

    Functions in react

--- vite-project-functions/src/input.jsx
+++ vite-project-functions/src/input.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const input = () => {
+
+    function inputChange(elem){
+        console.log(elem.target.value);
+    }
   return (
     <div>
-      <input type="text" placeholder="Enter your name" />
+      <input onChange={inputChange} type="text" placeholder="Enter your name" />
+      or
+      <input onChange={function(elem){
+        inputChange(elem.target.value)
+        // if we didnt't write in abover function elem thn we can do likew this
+      }} type="text" placeholder="Enter your name" />
     </div>
   )
 }
