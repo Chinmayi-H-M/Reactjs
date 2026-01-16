011ab7589f36dd5ad7789b0a186d164ab9bb4283
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-16 13:24:34 +0530

    Connecting pages

--- vite-project-tailwind/src/assets/components/section1/rightcontent.jsx
+++ vite-project-tailwind/src/assets/components/section1/rightcontent.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import RightCard from './rightcard.jsx'
 
-const rightcontent = () => {
+const rightcontent = (props) => {
   return (
-    <div className='h-full w-3/4 flex overflow-x-auto flex-nowrap bg-blue-600 p-10 gap-10'>
-      <RightCard/>
-      <RightCard/>
-      <RightCard/>
+    <div className='h-full w-2/3 flex overflow-x-auto flex-nowrap bg-blue-600 p-10 gap-10'>
+        {props.users.map(function(){
+          return <RightCard/>
+        })}
     </div>
   )
 }
