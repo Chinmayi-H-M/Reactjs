e5e5685b5c67211356fde9a17e535dc41ab69218
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-16 15:33:04 +0530

    Functions in react

--- vite-project-functions/src/input.jsx
+++ vite-project-functions/src/input.jsx
@@ -13,6 +13,14 @@ const input = () => {
         inputChange(elem.target.value)
         // if we didnt't write in abover function elem thn we can do likew this
       }} type="text" placeholder="Enter your name" />
+
+      <div onMouseMove={(elem)=>{
+        console.log(elem.clientY);
+      }} className="box">
+        onwheel
+        //delthaX nad deltaY for speed of mouse scroll
+
+      </div>
     </div>
   )
 }
