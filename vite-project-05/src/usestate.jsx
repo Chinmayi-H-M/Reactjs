4a1acdce8f85cca650b4dbbfb761226f666ee145
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-21 12:20:03 +0530

    Destructuring

--- vite-project-05/src/usestate.jsx
+++ vite-project-05/src/usestate.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const usestate = () => {
     const [num, setnum] = useState({user:'Chinmayi',age:20})
+    const [arr, setarr] = useState([10,20,30])
 
     const btnClicked=()=>{
         const newNum={...num};//Destructuring
@@ -10,9 +11,20 @@ const usestate = () => {
         setnum(newNum)
 
     }
+    const btnClicked1=()=>{
+        const newarr={...arr};//Destructuring
+        newarr.push(99)
+
+        setnum(newarr)
+
+    }
+    
   return (
     <div>
-      
+      <h1>{num.user},{num.age}</h1>
+      <button onclick={btnClicked}>Click</button>
+      <h1>{arr}</h1>
+      <button onclick={btnClicked1}>Click</button>
     </div>
   )
 }
