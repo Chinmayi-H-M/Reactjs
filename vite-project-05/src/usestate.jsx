54b98cfca3826d2fd84150f0f5697de4557dcf53
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-21 12:11:50 +0530

    Destructuring

---
+++ vite-project-05/src/usestate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const usestate = () => {
+    const [num, setnum] = useState({user:'Chinmayi',age:20})
+
+    const btnClicked=()=>{
+        const newNum={...num};
+        
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default usestate
