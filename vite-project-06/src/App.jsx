f66af89ecfd08e718971cad500aeb37248a4fba8
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-22 10:07:33 +0530

    Form validation

--- vite-project-06/src/App.jsx
+++ vite-project-06/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    console.log('Form submitted');
+  }
   return (
     <div>
-      
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder='Enter ur name'/>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
