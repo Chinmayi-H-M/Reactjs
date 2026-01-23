2ff57963fe98784dcd30f84506b2bb80fbaad226
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-23 11:18:00 +0530

    TWO WAY BINDING

--- vite-project-twb/src/App.jsx
+++ vite-project-twb/src/App.jsx
@@ -1,15 +1,22 @@
+
 const App = () => {
 
   const submitHandler = (e) =>{
     e.preventDefault()
     console.log('Form submitted');
   }
+  const [Title, setTitle] = useState(second)
   return (
     <div>
       <form onSubmit={(e)=>{
         submitHandler(e)
       }}>
-        <input type="text" placeholder='Enter ur name'/>
+        <input type="text" 
+        placeholder='Enter ur name'
+        onChange={(e)=>{
+          console.log(e.target.value);
+        }}
+        />
         <button>Submit</button>
       </form>
     </div>
