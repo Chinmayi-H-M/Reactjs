5203b63999546e647e00862dfe7a3f553b071165
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-24 16:44:34 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <form action="">
+      <form classname="flex items-center ">
         <input type="text" placeholder='Enter task heading' />
+        <textarea placeholder='Enter details' id="details" name='details'/>
       </form>
     </div>
   )
