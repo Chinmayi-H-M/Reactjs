a2b745a90c85d75825746c0f0576afc3049d6938
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-24 16:59:48 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
-      <form className="flex justify-between flex-col p-10">
+      <form className="flex justify-between flex-col p-10 gap-4">
         <input type="text" 
         placeholder='Enter task heading' 
-        className="p-2 border border-gray-300 rounded-md"/>
+        className="px-5 py-2 border border-gray-300 rounded-md"/>
         <input type="text"
         placeholder='Enter details' 
-        className='p-2 border border-gray-300 rounded-md'/>
+        className='px-5 py-2 h-20 border border-gray-300 rounded-md'/>
+        <button className="p-2 bg-blue-500 text-white rounded-md">Add Task</button>
       </form>
     </div>
   )
