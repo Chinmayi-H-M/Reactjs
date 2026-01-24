8c93b5f376bd63577e2b1ef106a1f4afdfa3a040
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-24 16:42:58 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <form action="">
+        <input type="text" placeholder='Enter task heading' />
+      </form>
     </div>
   )
 }
