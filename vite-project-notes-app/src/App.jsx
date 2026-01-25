3784887c21125051d7978c7923353639f01e7835
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-25 10:54:37 +0530

    Notes-app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -5,15 +5,15 @@ const App = () => {
     <div className='h-screen bg-black text-white'>
       <form className="flex items-start justify-between p-10 gap-4">
         <div className='flex flex-col p-5 items-start gap-4'>
-          <input type="text" 
-        placeholder='Enter task heading' 
-        className="px-5 py-2 border border-gray-300 rounded-md"/>
-        <input type="text"
-        placeholder='Enter details' 
-        className='px-5 py-2 h-20 border border-gray-300 rounded-md'/>
-        <button className="p-2 bg-blue-500 text-white rounded-md">Add Task</button>
+          <input type="text"
+            placeholder='Enter task heading'
+            className="px-5 py-2 border border-gray-300  outline-none rounded-md" />
+          <textarea type="text"
+            placeholder='Enter details'
+            className='px-5 py-2 h-20 border border-gray-300 outline-none rounded-md' />
+          <button className="p-2 bg-blue-500 text-white rounded-md">Add Task</button>
         </div>
-        <img  className="h-60" src="https://pngimg.com/uploads/notebook/notebook_PNG19227.png" alt="" />
+        <img className="h-60" src="https://pngimg.com/uploads/notebook/notebook_PNG19227.png" alt="" />
       </form>
     </div>
   )
