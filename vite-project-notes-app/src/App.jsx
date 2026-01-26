139213e297c56fba32dcfefcdd1b4a2bb1f59fb4
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-26 08:23:53 +0530

    Notes-app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -10,7 +10,8 @@ const App = () => {
     <form onSubmit={(e)=>{
         submitHandler(e);
       }
-    }className="flex items-start lg:w-1/2 p-10 gap-4">
+    }className="flex flex-col items-start lg:w-1/2 p-10 gap-4">
+      <h1 className="text-3xl font-bold">Add Notes</h1>
         <div className='flex flex-col p-5 items-start gap-4'>
           <input type="text"
             placeholder='Enter task heading'
@@ -20,7 +21,7 @@ const App = () => {
             className='px-5 py-2 h-20 border border-gray-300 outline-none rounded-md' />
           <button className="p-2 bg-blue-500 text-white rounded-md">Add Task</button>
         </div>
-        <img className="h-60" src="https://pngimg.com/uploads/notebook/notebook_PNG19227.png" alt="" />
+        
       </form>
       <div className="lg:w-1/2 lg:border-l-2 bg-gray-900 p-10">
         <h1 className="text-2xl font-bold">Your Tasks</h1>
