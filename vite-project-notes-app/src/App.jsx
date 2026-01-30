c2746946b7d5430d0667c58f1e7c4aad817754d9
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-30 18:10:09 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
         <h1 className="text-2xl font-bold">Recent Tasks</h1>
         <div className='flex flex-wrap items-start gap-4  justify-start mt-5 overflow-auto min-h-full'>
            {tasks.map(function(task,idx){
-            return <div key={idx} className="h-52 w-40 rounded-2xl text-black bg-white py-9 px-4 bg-[url('https://cdn.pixabay.com/photo/2017/03/18/17/46/notepad-2154581_640.png')] bg-cover">
+            return <div key={idx} className="h-52 w-45 rounded-2xl text-black bg-white py-13 px-4 bg-[url('https://cdn.pixabay.com/photo/2017/03/18/17/46/notepad-2154581_640.png')] bg-cover bg-center ">
               <h2 className="font-bold">{task.title}</h2>
               <p className='mt-4 leading-tight font-medium text-gray-500'>{task.details}</p>
             </div>
