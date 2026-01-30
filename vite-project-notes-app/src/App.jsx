4ce0ea5747d42509192367d83f23843cb2ea9254
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-30 18:56:23 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -53,7 +53,7 @@ const App = () => {
                 <h2 className="font-bold">{task.title}</h2>
                 <p className='mt-4 leading-tight font-medium text-gray-500'>{task.details}</p>
               </div>
-                <button className="p-2 active:bg-gray-400 bg-red-500 text-white rounded-md">Delete Note</button>
+                <button className="w-full p-2 active:bg-gray-400 bg-red-500 text-white rounded-md">Delete Note</button>
               </div>
            })}
             </div>
