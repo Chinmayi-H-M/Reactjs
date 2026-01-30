b0f03edda30305818c24ad03dcec18a7b812bb1e
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-30 18:51:22 +0530

    Notes app

--- vite-project-notes-app/src/App.jsx
+++ vite-project-notes-app/src/App.jsx
@@ -3,60 +3,63 @@ import React, { useState } from 'react'
 const App = () => {
   const [title, setTitle] = useState("")
   const [details, setDetails] = useState("")
-  const [tasks,setTasks]=useState([])
-  const submitHandler=(e)=>{
+  const [tasks, setTasks] = useState([])
+  const submitHandler = (e) => {
     e.preventDefault();
     alert("Task added successfully!");
     console.log("Ur task added successfully");
-    const copyTasks=[...tasks];
-    copyTasks.push({title,details});
+    const copyTasks = [...tasks];
+    copyTasks.push({ title, details });
     setTasks(copyTasks);
     setTitle("");
     setDetails("");
   }
-  
+
   return (
     <div className='h-screen lg:flex bg-black text-white'>
-    <form onSubmit={(e)=>{
+      <form onSubmit={(e) => {
         submitHandler(e);
       }
-      
-    }className="flex flex-col items-start lg:w-1/2 p-10 gap-4">
-      <h1 className="text-3xl font-bold">Add Notes</h1>
+
+      } className="flex flex-col items-start lg:w-1/2 p-10 gap-4">
+        <h1 className="text-3xl font-bold">Add Notes</h1>
         <div className='flex flex-col p-5 items-start gap-4'>
           <input type="text"
             placeholder='Enter task heading'
-            className="px-5 py-2 border border-gray-300  outline-none rounded-md" 
+            className="px-5 py-2 border border-gray-300  outline-none rounded-md"
             value={title}
-            onChange={(e)=>{
+            onChange={(e) => {
               setTitle(e.target.value);
             }}
-            />
+          />
           <textarea type="text"
             placeholder='Enter details'
             className='px-5 py-2 h-20 border border-gray-300 outline-none rounded-md'
             value={details}
-            onChange={(e)=>{
+            onChange={(e) => {
               setDetails(e.target.value);
             }}
-            />
+          />
           <button className="p-2 active:bg-gray-400 bg-blue-500 text-white rounded-md">Add Task</button>
         </div>
-        
+
       </form>
       <div className="lg:w-1/2 lg:border-l-2 bg-gray-900 p-10">
         <h1 className="text-2xl font-bold">Recent Tasks</h1>
         <div className='flex flex-wrap items-start gap-4  justify-start mt-5 overflow-auto min-h-full'>
-           {tasks.map(function(task,idx){
+          {tasks.map(function (task, idx) {
             return <div key={idx} className="h-52 w-45 rounded-2xl text-black bg-white py-13 px-4 bg-[url('https://cdn.pixabay.com/photo/2017/03/18/17/46/notepad-2154581_640.png')] bg-cover bg-center ">
-              <h2 className="font-bold">{task.title}</h2>
-              <p className='mt-4 leading-tight font-medium text-gray-500'>{task.details}</p>
-            </div>
+              <div>
+                <h2 className="font-bold">{task.title}</h2>
+                <p className='mt-4 leading-tight font-medium text-gray-500'>{task.details}</p>
+              </div>
+                <button className="p-2 active:bg-gray-400 bg-red-500 text-white rounded-md">Delete Note</button>
+              </div>
            })}
-        </div>
+            </div>
       </div>
-    </div>
-  )
+      </div>
+      )
 }
 
-export default App
+      export default App
