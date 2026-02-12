15c8c205b21ae78ba2f8ec9e1c5c612bf33ebd8e
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-12 21:11:40 +0530

    Navbar

--- vite-project-landing-page/src/components/Navbar.jsx
+++ vite-project-landing-page/src/components/Navbar.jsx
@@ -6,6 +6,13 @@ const Navbar = ({theme,setTheme}) => {
     <div>
       <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
       <img src={theme==="dark" ? assets.logo_dark : assets.logo} className='w-32 sm:w-40' alt="" />
+
+      <div>
+        <a href="#" className="sm:hover:border-b">Home</a>
+        <a href="#services" className="sm:hover:border-b">Services</a>
+        <a href="#our-work" className="sm:hover:border-b">Our Work</a>
+        <a href="#contact-us" className="sm:hover:border-b">Home</a>
+      </div>
       </div>
     </div>
   )
