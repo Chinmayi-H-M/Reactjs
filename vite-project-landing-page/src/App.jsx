b8b5a0ce89f425047acbf4e267901be19b16e257
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-13 13:33:46 +0530

    NavBar

--- vite-project-landing-page/src/App.jsx
+++ vite-project-landing-page/src/App.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Navbar from './components/Navbar'
 
 
 const App = () => {
-  const[theme, setTheme] = useState('light')
+  const[theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+  useEffect(()=>{
+        if(theme==='dark'){
+            document.documentElement.classList.add('dark')
+        }
+        else{
+            document.documentElement.classList.remove('dark')
+        }
+        localStorage.setItem('theme',theme)
+    },[theme])
   return (
-    <div className={`${theme} relative`}>
+    <div className="bg-white dark:bg-black min-h-screen transition-colors duration-300">
+
       <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
--- vite-project-landing-page/src/components/Theme.jsx
+++ vite-project-landing-page/src/components/Theme.jsx
@@ -2,23 +2,16 @@ import React, { useEffect } from 'react'
 import assets from '../assets/assets'
 
 const Theme = ({theme,setTheme}) => {
-    useEffect(()=>{
-        if(theme==='dark'){
-            document.documentElement.classList.add('dark')
-        }
-        else{
-            document.documentElement.classList.remove('dark')
-        }
-        localStorage.setItem('theme',theme)
-    },[theme])
+    
+    
   return (
     <>
-      <button>
+      <button onClick={()=>setTheme(theme==='dark' ? 'light' : 'dark')}>
         {theme==="dark" ? (
-            <img onClick={()=>setTheme('light')} src={assets.sun_icon} 
+            <img src={assets.sun_icon} 
             className='size-8.5 p-1.5 border border-gray-500 rounded-full' alt="" />
         ) : (
-            <img onClick={()=>setTheme('dark')} src={assets.moon_icon} className="size-8.5 p-1.5 border border-gray-500 rounded-full "alt=""/>
+            <img src={assets.moon_icon} className="size-8.5 p-1.5 border border-gray-500 rounded-full "alt=""/>
         )}
       </button>
     </>
