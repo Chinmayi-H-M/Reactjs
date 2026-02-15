fe52a8d17f0a54b4047efd4837ed7424a4cb590e
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-15 22:30:50 +0530

    our work section

---
+++ vite-project-landing-page/src/components/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from './Title'
+
+const ContactUs = () => {
+  return (
+    <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <Title title="Reach out to us" desc="Have a project in mind? We would love to hear from you. Whether you have a an idea, a question, or just want to say hi, feel free to reach out to us. We are always open to discussing new projects, creative ideas, or opportunities to be part of your visions. Our team is ready to collaborate and bring your ideas to life."/>
+      
+    </div>
+  )
+}
+
+export default ContactUs
--- vite-project-landing-page/src/components/OurWork.jsx
+++ vite-project-landing-page/src/components/OurWork.jsx
@@ -23,8 +23,8 @@ const OurWork = () => {
 
   ]
   return (
-    <div id='ourwork' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 pt-30 text-gray-700 dark:text-white'>
-        <Title title="Our Latest Work" description="Here are some of our latest projects. We have worked on a variety of projects and have a wide range of expreriance. We are always looking for new and exiting projects to work on. If you have a project in mind, feel free to contact us."/>
+    <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 pt-30 text-gray-700 dark:text-white'>
+        <Title title="Our Latest Work" desc="Here are some of our latest projects. We have worked on a variety of projects and have a wide range of expreriance. We are always looking for new and exiting projects to work on. If you have a project in mind, feel free to contact us."/>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
             {
