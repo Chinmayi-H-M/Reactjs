6756d4a22a5d778e1f384e06480cae95090227cf
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-15 15:45:05 +0530

    our work section

--- vite-project-landing-page/src/components/OurWork.jsx
+++ vite-project-landing-page/src/components/OurWork.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Title from './Title'
 
 const OurWork = () => {
+    
+
   return (
-    <div id='ourwork' className='flex flex-col' >
+    <div id='ourwork' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 pt-30 text-gray-700 dark:text-white'>
+        <Title title="Our Latest Work" description="Here are some of our latest projects. We have worked on a variety of projects and have a wide range of expreriance. We are always looking for new and exiting projects to work on. If you have a project in mind, feel free to contact us."/>
       
     </div>
   )
