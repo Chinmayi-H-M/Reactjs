1282123665a7fb496de9a8a196624f3fce165704
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-15 22:09:29 +0530

    our work section

--- vite-project-landing-page/src/App.jsx
+++ vite-project-landing-page/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
+import Teams from './components/Teams'
 
 const App = () => {
   const [theme, setTheme] = useState(() => {
@@ -29,6 +30,7 @@ const App = () => {
       <TrustedBy />
       <Services />
       <OurWork/>
+      <Teams />
 
     </div>
   )
