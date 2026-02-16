81675c62d3f90f853bfa3a62546020a36a689bd2
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-16 21:24:54 +0530

    LocalStorage in react

--- vite-project-storage/src/App.jsx
+++ vite-project-storage/src/App.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 const App = () => {
   localStorage.setItem("name","Chinmayi");
   localStorage.getItem("name");
+  localStorage.removeItem("name");
+  
+  
+  const user={
+    username:"Chinmayi",
+    age:20,
+    city:"Bhopal"
+  }
+  localStorage.setItem("user",JSON.stringify(user));
+
+
+  
 
   return (
     <div>
