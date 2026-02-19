2ad0df3fb819326a4c909fb7153c8291b1945022
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-19 19:10:00 +0530

    Routers in react

--- vite-project-router/src/App.jsx
+++ vite-project-router/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
 import Contact from './pages/Contact'
+import About from './pages/about'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
-      </Routes>
+        <Route path='/about' element={<About />} />
+      </Routes> 
     </div>
   )
 }
