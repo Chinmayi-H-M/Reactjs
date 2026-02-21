b1a2476b50319f4a839ee1bbff7bc55de6f32a73
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-21 22:39:43 +0530

    Context api

--- vite-project-bonus-topic/src/components/Navbar.jsx
+++ vite-project-bonus-topic/src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import Navbar2 from './Navbar2'
 
+const Navbar = (props) => {
+  return (
+    <div className='nav'>
+        <h2>My App</h2>
+        <Navbar2 theme={props.theme} setTheme={props.setTheme}/>
+      
+    </div>
+  )
+}
+
+export default Navbar
